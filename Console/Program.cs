84ca53d789358f316fac9fa86e7abb43f4c4acfc
tonyfages <tony.fages@etu.uca.fr>
2026-02-02 14:38:33 +0100

    Clean :clean:

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,34 +4,27 @@ using Easy.Log.Interface;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine();
-Console.WriteLine("--- Démarrage du test EasySave ---");
+Console.WriteLine("--- Démarrage du test  ---");
 
-// 1. Définir l'endroit où on veut stocker les logs
-// Directory.GetCurrentDirectory() pointe vers le dossier bin/Debug/net8.0/ lors du run
 string logsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "logs");
 
-// 2. Instancier le Logger
-// On utilise l'interface (ILogger) pour la déclaration, c'est une bonne pratique (polymorphisme)
+
 ILogger myLogger = new DailyLogsService();
 
-// 3. Créer une fausse donnée de log (Simule un travail de sauvegarde fini)
-LogData testLog = new LogDataCustom()
+var testLog = new LogData()
 {
     Timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"),
     BackupJobName = "Travail_Test_Alpha",
     SourceFilePath = @"C:\Utilisateurs\Moi\Documents\Projet.doc",
     TargetFilePath = @"D:\Backups\Projet.doc",
-    FileSize = 4500,          // 4.5 Ko
-    TransferTimeMs = 125,     // 125 ms
+    FileSize = 4500,
+    TransferTimeMs = 125,
     EncryptionTimeMs = 0,
 };
 
 try
 {
     myLogger.WriteInFile(logsDirectory, testLog);
-    Console.WriteLine($"YEYYYYY");
-
-
 }
 catch (Exception ex)
 {
