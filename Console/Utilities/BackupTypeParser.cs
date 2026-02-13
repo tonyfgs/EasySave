dbb0a39aac83320d7f709fb074bf6d4a706fc303
David d'Almeida <daivd@MacBook-Pro-de-David.local>
2026-02-13 13:03:39 +0100

    test: add failing tests for BackupTypeParser rejecting numeric input (#51)

---
+++ Console/Utilities/BackupTypeParser.cs
@@ -0,0 +1,11 @@
+using Model;
+
+namespace EasySave.Utilities;
+
+public static class BackupTypeParser
+{
+    public static BackupType Parse(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
